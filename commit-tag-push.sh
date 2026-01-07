ce96362d8bfa9c076cc63627177619164e3a5015
Cevher Dogan <cevherd@gmail.com>
2026-01-07 13:48:37 -0500

    init: bootstrap from Foundral repo template v1.9.9

---
+++ commit-tag-push.sh
@@ -0,0 +1,6 @@
+git add . 
+git commit -m "v4.1: release notes + config walkthrough + ops runbook"
+git tag -a v4.1 -m "Foundral Investment Atlas v4.1"
+git push -u origin main
+git push origin --tags
+
---
+++ ops/using-template-instructions.sh
@@ -0,0 +1,9 @@
+unzip foundral_repo_template_v1_9_9.zip
+mv foundral-repo-template-v1.9.9 your-new-repo
+cd your-new-repo
+
+git init
+git add .
+git commit -m "init: bootstrap from Foundral repo template v1.9.9"
+git branch -M main
+
